ec78756b6a628926e1ccdefc1b06734c7052cfcb
siweimutong <35714039+siweimutong@users.noreply.github.com>
2026-01-01 13:57:25 +0000

    Deploying to gh-pages from @ siweimutong/siweimutong.github.io@d987671648d21cfb4531c42f814189f4b90272c4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -392,14 +392,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6C%75%6F%78%69%61%6E%74%6F%6E%67@%62%6A%75%74.%65%64%75.%63%6E", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/siweimutong", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0003-4667-0619", "_blank");
         },
       },{
         id: 'social-rss',
@@ -413,7 +420,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=O5Yg3hoAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
