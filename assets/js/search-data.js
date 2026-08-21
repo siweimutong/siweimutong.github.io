82212cbddc00c9cd6b81ceb7004e835c32ef1f77
siweimutong <35714039+siweimutong@users.noreply.github.com>
2026-08-21 06:36:48 +0000

    Deploying to gh-pages from @ siweimutong/siweimutong.github.io@f4c7b380f161392ea580c4450b3f59b0ed4bf64c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "My open-source projects.",
+          description: "Selected research projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
