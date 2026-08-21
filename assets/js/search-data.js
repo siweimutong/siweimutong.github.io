c79578f94f7295560b2196758536633a47a62709
siweimutong <35714039+siweimutong@users.noreply.github.com>
2026-08-21 06:20:53 +0000

    Deploying to gh-pages from @ siweimutong/siweimutong.github.io@4a9fff7f2ea42b586d8a45cf928e4d3122fe3361 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,19 +16,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "My open-source projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-collaborators",
           title: "Collaborators",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cooperators/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
           },
         },{id: "nav-students",
           title: "Students",
